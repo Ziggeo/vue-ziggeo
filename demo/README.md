d34f07e2e9ab3529e9b742cd0f4d344188bf1c1e
Rashad Aliyev <aliyev.resad@gmail.com>
2018-01-07 20:34:10 +0400

    Complete library integration, worked with demo

--- demo/README.md
+++ demo/README.md
@@ -1,6 +1,6 @@
 # vue-ziggeo-demo
 
-> Vue.js Ziggeo awesome api integration
+> Vuejs and Ziggeo Demo
 
 ## Build Setup
 
@@ -13,6 +13,9 @@ npm run dev
 
 # build for production with minification
 npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
 ```
 
-For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- demo/src/App.vue
+++ demo/src/App.vue
@@ -3,23 +3,56 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Ziggeo Player</h2>
-    <ziggeo-player
-        :apiToke="'asdasdasdasdasdasd'"
-    >
-    </ziggeo-player>
+      <ziggeo-player
+          :apiKey="'ZIGGEO_API_KEY'"
+          :video="'VIDEO_TOKEN'"
+          :width="330"
+          :height="210"
+          @attached="playerAttached"
+          @playing="playerPlaying"
+      ></ziggeo-player>
+
+    <h2>Ziggeo Recorder</h2>
+    <ziggeo-recorder
+        :apiKey="'ZIGGEO_API_KEY'"
+        :width="110"
+        :height="80"
+        @camera_unresponsive="recorderCameraUnresponsive"
+        @access_forbidden="recorderAccessForbidden"
+        @upload_selected="recorderUploadSelected"
+    ></ziggeo-recorder>
   </div>
 </template>
 
 <script>
-    import ZiggeoPlayer from '../../build/index';
+
     export default {
         name: 'app',
-        components: {
-            "ziggeo-player": ZiggeoPlayer
-        },
         data () {
             return {
-                msg: 'Welcome to Your Vue.js App'
+                msg: 'Welcome to Your Vue.js And Ziggeo App integration demo'
+            }
+        },
+        methods: {
+
+            playerAttached: function (data) {
+                console.log('player attached', data)
+            },
+
+            playerPlaying: () => {
+                console.log('player playing');
+            },
+
+            recorderCameraUnresponsive: function() {
+                console.log('camera unresponsive');
+            },
+
+            recorderAccessForbidden: function () {
+                console.log('access forbidden');
+            },
+
+            recorderUploadSelected: (file) => {
+                console.log('upload selected', file);
             }
         }
     }
